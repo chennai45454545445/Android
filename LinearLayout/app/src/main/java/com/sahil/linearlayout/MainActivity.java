f848b00310571b3733d7a0963b26183e33072184
Mohammed sahil ansari <mrsahilansari2002@gmail.com>
2021-12-24 10:32:39 +0530

    Android Dovelopment
    Android

---
+++ LinearLayout/app/src/main/java/com/sahil/linearlayout/MainActivity.java
@@ -0,0 +1,30 @@
+package com.sahil.linearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        Button button;
+
+        button=findViewById(R.id.button);
+
+                    button.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent=new Intent(MainActivity.this,Relative2.class);
+                            startActivity(intent);
+                        }
+                    });
+    }
+}
---
+++ LinearLayout/app/src/main/java/com/sahil/linearlayout/Relative2.java
@@ -0,0 +1,14 @@
+package com.sahil.linearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Relative2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative2);
+    }
+}
